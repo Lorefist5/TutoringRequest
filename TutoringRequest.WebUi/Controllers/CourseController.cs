635940e78ee645bdba69afa6cc9e0b2b3e8b7359
Andres <andresitomanuel@gmail.com>
2024-03-23 16:51:03 -0300

    Courses controller and Views added

--- TutoringRequest.WebUi/Controllers/CourseController.cs
+++ TutoringRequest.WebUi/Controllers/CourseController.cs
@@ -6,7 +6,6 @@ using TutoringRequest.Services.HttpClientServices;
 using TutoringRequest.WebUi.ViewModel;
 
 namespace TutoringRequest.WebUi.Controllers;
-
 public class CourseController : Controller
 {
     private readonly CourseApiService _courseApiService;
@@ -17,54 +16,89 @@ public class CourseController : Controller
         this._courseApiService = courseApiService;
         this._majorApiService = majorApiService;
     }
-    public async Task<IActionResult> Index()
+    public async Task<IActionResult> Index(Guid? majorId, string searchQuery = "")
     {
-        var response = await _courseApiService.GetAllAsync<CourseDto>();
-        if (!response.IsSuccess)
+        var coursesResponse = majorId.HasValue ?
+            await _majorApiService.GetCoursesByMajor(majorId.Value) :
+            await _courseApiService.GetAllAsync<CourseDto>(); //If a major was selected as a filter it will get all the courses from that major if not it will get all the courses
+        var majorsResponse = await _majorApiService.GetAllAsync<MajorDto>();
+
+        if (!coursesResponse.IsSuccess || !majorsResponse.IsSuccess)
         {
             return RedirectToAction("Index", "Home");
         }
-        List<CourseDto> courses = response.Values!;
-        return View(courses);
+
+        ViewBag.Majors = majorsResponse.Values!;
+        ViewBag.SelectedMajorId = majorId;
+        List<CourseDto> courses = coursesResponse.Values!;
+        List<MajorDto> majors = majorsResponse.Values!;
+
+        // Filter courses by the search query if it's not empty
+        if (!string.IsNullOrWhiteSpace(searchQuery))
+        {
+            courses = courses.Where(c => c.CourseName.Contains(searchQuery, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+
+        IEnumerable<CourseViewModel> coursesViewModel = courses.Select(course =>
+        {
+            MajorDto? major = majors.FirstOrDefault(m => m.Id == course.MajorId);
+            return new CourseViewModel(course, major);
+        });
+
+        return View(coursesViewModel);
     }
+
     [Authorize(Roles = "Admin")]
     public async Task<IActionResult> Delete(CourseDto courseDto)
     {
         return View(courseDto);
     }
+    [HttpPost]
     [Authorize(Roles = "Admin")]
-    [HttpDelete]
     public async Task<IActionResult> Delete(Guid id)
     {
-        return View();
+        var response = await _courseApiService.DeleteAsync(id.ToString());
+        if (!response.IsSuccessStatusCode)
+        {
+            return View();
+        }
+        TempData["SuccessMessage"] = "Course deleted successfully!";
+        return RedirectToAction("Index", "Course");
     }
     [Authorize(Roles = "Admin")]
-    public async Task<IActionResult> Create()
+    public async Task<IActionResult> Create(Guid? majorId)
     {
         var response = await _majorApiService.GetAllAsync<MajorDto>();
         if (!response.IsSuccess)
         {
             return RedirectToAction("Index", "Home");
         }
-
-        var viewModel = new CourseViewModel
+        if(majorId != null)
+        {
+            ViewBag.SelectedMajorId = majorId;
+        }
+        var viewModel = new AddCourseViewModel
         {
             Majors = response.Values!
         };
 
         return View(viewModel);
     }
-
     [Authorize(Roles = "Admin")]
     [HttpPost]
-    public async Task<IActionResult> Create(CourseViewModel courseViewModel)
+    public async Task<IActionResult> Create(AddCourseViewModel courseViewModel)
     {
         var response = await _courseApiService.PostAsync(courseViewModel.AddCourseDto);
         if (!response.IsSuccessStatusCode)
         {
             return View();
         }
-        return RedirectToAction("Create", "Course");
+        TempData["SuccessMessage"] = "Course created successfully!";
+        return RedirectToAction("Index", "Course");
     }
-
+    [Authorize(Roles = "Student")]
+    public async Task<IActionResult> Schedule()
+    {
+        return View();
+    }
 }
