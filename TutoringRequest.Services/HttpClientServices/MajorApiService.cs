419df17edd88bced428c570c116f8974341961f9
Andres <andresitomanuel@gmail.com>
2024-03-23 16:42:28 -0300

    Major GetCoursesByMajor method added

--- TutoringRequest.Services/HttpClientServices/MajorApiService.cs
+++ TutoringRequest.Services/HttpClientServices/MajorApiService.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using TutoringRequest.Models.Attributes;
+using TutoringRequest.Models.DTO.Courses;
+using TutoringRequest.Models.DTO.Http;
 using TutoringRequest.Services.HttpClientServices.Base;
 
 namespace TutoringRequest.Services.HttpClientServices;
@@ -9,4 +11,17 @@ public class MajorApiService : GenericApiService
     public MajorApiService(HttpClient httpClient, IHttpContextAccessor httpContextAccessor, string? defaultRoute = null) : base(httpClient, httpContextAccessor, defaultRoute)
     {
     }
+    public async Task<HttpResponse<CourseDto>> GetCoursesByMajor(Guid majorId)
+    {
+        string url = $"{_defaultRoute}/{majorId}/courses";
+
+        var courses = await GetAsync<List<CourseDto>>(url);
+        var response = new HttpResponse<CourseDto>()
+        {
+            Values = courses ?? new List<CourseDto>(),
+            IsSuccess = true
+        };
+        return response;
+    }
+
 }
