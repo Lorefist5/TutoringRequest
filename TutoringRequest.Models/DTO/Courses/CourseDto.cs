f9c2cf88c68cab99fd642e0f0726ccd2ca8dea48
Andres <andresitomanuel@gmail.com>
2024-03-23 16:36:27 -0300

    CourseDto should include the Id of the major

--- TutoringRequest.Models/DTO/Courses/CourseDto.cs
+++ TutoringRequest.Models/DTO/Courses/CourseDto.cs
@@ -1,7 +1,11 @@
-﻿namespace TutoringRequest.Models.DTO.Courses;
+﻿using TutoringRequest.Models.Domain;
+using TutoringRequest.Models.DTO.Majors;
+
+namespace TutoringRequest.Models.DTO.Courses;
 
 public class CourseDto
 {
     public Guid Id { get; set; }
     public string CourseName { get; set; } = default!;
+    public Guid MajorId { get; set; }
 }
