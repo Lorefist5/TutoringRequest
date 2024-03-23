0ea7e10d0d43af26a1b2cfcad788bead0a2c273c
Andres <andresitomanuel@gmail.com>
2024-03-23 16:46:25 -0300

    Students and Tutors controller added

---
+++ TutoringRequest.WebUi/Controllers/StudentController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TutoringRequest.WebUi.Controllers;
+
+public class StudentController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
