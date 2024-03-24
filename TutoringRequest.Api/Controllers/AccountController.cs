f0e99564fa8283034e83b8eb5717e3e7b37d6416
Andres <andresitomanuel@gmail.com>
2024-03-24 18:18:53 -0300

    Get and change account info methods created

--- TutoringRequest.Api/Controllers/AccountController.cs
+++ TutoringRequest.Api/Controllers/AccountController.cs
@@ -1,7 +1,10 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using TutoringRequest.Api.Helpers.TokenHelpers;
 using TutoringRequest.Data.Repositories.Interfaces;
 using TutoringRequest.Models.Domain;
+using TutoringRequest.Models.DTO.Accounts;
 using TutoringRequest.Models.DTO.Roles;
 
 namespace TutoringRequest.Api.Controllers;
@@ -12,11 +15,13 @@ public class AccountController : ControllerBase
 {
     private readonly IUnitOfWork _unitOfWork;
     private readonly IMapper _mapper;
+    private readonly TokenGenerator _tokenGenerator;
 
-    public AccountController(IUnitOfWork unitOfWork, IMapper mapper)
+    public AccountController(IUnitOfWork unitOfWork, IMapper mapper, TokenGenerator tokenGenerator)
     {
         this._unitOfWork = unitOfWork;
         this._mapper = mapper;
+        this._tokenGenerator = tokenGenerator;
     }
     [HttpGet("roles/{id:guid}")]
     public async Task<IActionResult> GetAccountRoles(Guid id)
@@ -45,6 +50,107 @@ public class AccountController : ControllerBase
             return Ok();
         }
 
+    }
+    [Authorize]
+    [HttpGet("info/{userId:guid}")]
+    public async Task<IActionResult>GetUserInfo(Guid userId)
+    {
+        Account? user = await _unitOfWork.AccountRepository.FirstOrDefaultAsync(a => a.Id == userId);
+        if(user == null) return NotFound();
+
+        var userInfo = new AccountInfoDto()
+        {
+            Id = userId,
+            Email = user.Email,
+            PhoneNumber = user.PhoneNumber,
+            Roles = user.Roles.Select(r => r.RoleName),
+            StudentNumber = user.StudentNumber,
+            UserName = user.Name
+        };
+
+        return Ok(userInfo);
+    }
+
+    [Authorize]
+    [HttpPost("change-username/{userId:guid}")]
+    public async Task<IActionResult> ChangeUserName(Guid userId,[FromBody] string newUserName)
+    {
+        Account? user = await _unitOfWork.AccountRepository.FirstOrDefaultAsync(user => user.Id == userId);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        user.Name = newUserName;
+        user.UpdatedAt = DateTime.UtcNow;
+        await _unitOfWork.SaveChangesAsync();
+
+        var token = _tokenGenerator.GenerateJwtToken(user);
+
+
+        return Ok(new { Token = token });
+    }
+    [Authorize]
+    [HttpPost("change-email")]
+    public async Task<IActionResult> ChangeEmail([FromBody] ChangeEmailDto changeEmailDto)
+    {
+        Account? user = await _unitOfWork.AccountRepository.FirstOrDefaultAsync(user => user.Email == changeEmailDto.OldEmail);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        user.Email = changeEmailDto.NewEmail;
+        user.UpdatedAt = DateTime.UtcNow;
+        await _unitOfWork.SaveChangesAsync();
+
+        var token = _tokenGenerator.GenerateJwtToken(user);
+
+
+        return Ok(new { Token = token });
+    }
+
+    [Authorize]
+    [HttpPost("change-phone/{userId:guid}")]
+    public async Task<IActionResult> ChangePhoneNumber(Guid userId,[FromBody] string newPhoneNumber)
+    {
+        Account? user = await _unitOfWork.AccountRepository.FirstOrDefaultAsync(user => user.Id == userId);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        user.PhoneNumber = newPhoneNumber;
+        user.UpdatedAt = DateTime.UtcNow;
+        await _unitOfWork.SaveChangesAsync();
+
+        var token = _tokenGenerator.GenerateJwtToken(user);
+
+
+        return Ok(new { Token = token });
+    }
+    [Authorize]
+    [HttpPost("change-password/{userId:guid}")]
+    public async Task<IActionResult> ChangePassword(Guid userId, [FromBody] ChangePasswordDto changePasswordDto)
+    {
+        Account? user = await _unitOfWork.AccountRepository.FirstOrDefaultAsync(user => user.Id == userId);
+        if (user == null)
+        {
+            return NotFound("User not found.");
+        }
+
+        if (!BCrypt.Net.BCrypt.Verify(changePasswordDto.OldPassword, user.Password))
+        {
+            return BadRequest("The old password is incorrect.");
+        }
+
+        user.Password = BCrypt.Net.BCrypt.HashPassword(changePasswordDto.NewPassword);
+        user.UpdatedAt = DateTime.UtcNow;
+        await _unitOfWork.SaveChangesAsync();
+
+        var token = _tokenGenerator.GenerateJwtToken(user);
+
+        return Ok(new { Token = token });
     }
 
 }
